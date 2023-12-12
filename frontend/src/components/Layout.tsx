98156e2b8516d1d00acc43dd73bd99063fa6904f
LoganjwSim <j1sim234@gmail.com>
2023-12-12 19:55:36 +0900

    Second Commit

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,14 +1,46 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import Web3, { Contract, ContractAbi } from "web3";
+import { useSDK } from "@metamask/sdk-react";
+
 import Header from "./Header";
+import mintNftAbi from "../abis/mintNftAbi.json";
+
+export interface OutletContext {
+  account: string;
+  web3: Web3;
+  mintNftContract: Contract<ContractAbi>;
+}
 
 const Layout: FC = () => {
   const [account, setAccount] = useState<string>("");
+  const [web3, setWeb3] = useState<Web3>();
+  const [mintNftContract, setMintNftContract] =
+    useState<Contract<ContractAbi>>();
+
+  const { provider } = useSDK();
+
+  useEffect(() => {
+    if (!provider) return;
+
+    setWeb3(new Web3(provider));
+  }, [provider]);
+
+  useEffect(() => {
+    if (!web3) return;
+
+    setMintNftContract(
+      new web3.eth.Contract(
+        mintNftAbi,
+        "0xe1602e1c9e86ef91d13f9571f99099af101e1007"
+      )
+    );
+  }, [web3]);
 
   return (
-    <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto">
+    <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex flex-col">
       <Header account={account} setAccount={setAccount} />
-      <Outlet />
+      <Outlet context={{ account, web3, mintNftContract }} />
     </div>
   );
 };
--- frontend/src/pages/my.tsx
+++ frontend/src/pages/my.tsx
@@ -1,7 +1,32 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { OutletContext } from "../components/Layout";
+import MintModal from "../components/MintModal";
 
 const My: FC = () => {
-    return <div>My</div>;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const { mintNftContract } = useOutletContext<OutletContext>();
+
+  useEffect(() => {
+    console.log(mintNftContract);
+  }, [mintNftContract]);
+
+  return (
+    <>
+      <div className="bg-green-100 grow">
+        <div className="bg-purple-100 text-right p-2">
+          <button
+            className="hover:text-gray-500"
+            onClick={() => setIsOpen(true)}
+          >
+            Mint
+          </button>
+        </div>
+      </div>
+      {isOpen && <MintModal setIsOpen={setIsOpen} />}
+    </>
+  );
 };
 
 export default My;
